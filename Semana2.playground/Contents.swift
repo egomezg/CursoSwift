77a7cb7fbb732727bcb5d28f9dff988d878eb42a
Edgar <egomezg@gmail.com>
2015-12-10 20:29:30 -0600

    Se agregaron comentarios

--- Semana2.playground/Contents.swift
+++ Semana2.playground/Contents.swift
@@ -5,14 +5,15 @@ import UIKit
 var rango = 0...100 //Declaración de rango
 
 for cont in rango{
-    if (cont >= 30) && (cont <= 40) {
+    if (cont >= 30) && (cont <= 40) { // Impresión de Viva Swift 30-40
         print("#\(cont) Viva Swift!!!")
-    }else if (cont%5 == 0) && (cont != 0){
+    }else if (cont%5 == 0) && (cont != 0){ // Impresión Bingo div entre 5 sin el 0
         print("#\(cont) Bingo!!!")
-    }else if (cont%2 == 0) {
+    }else if (cont%2 == 0) { // Impresión de Numero Par
         print("#\(cont) es Par!!!")
-    }else if (cont%2 != 0) {
+    }else if (cont%2 != 0) { //Impresión Numero Impar
         print("#\(cont) es Impar!!!")
     }
     
 }
+
